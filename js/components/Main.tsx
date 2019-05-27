11b66841e236e1350fe423cfaebcb3803b6fa464
Paweł Wegner <pawel.wegner95@gmail.com>
2019-05-27 15:17:17 +0200

    Main: added drawer.

--- js/components/Main.tsx
+++ js/components/Main.tsx
@@ -4,13 +4,25 @@ import { CloudFactory } from "../cloudstorage";
 
 import { AppBar } from 'react-toolbox/lib/app_bar';
 import { Layout, NavDrawer, Panel } from 'react-toolbox/lib/layout';
+import { List, ListItem, ListSubHeader, ListDivider, ListCheckbox } from 'react-toolbox/lib/list';
 
-export interface MainState { content: string, authorizeUrl: string, providers: string }
+export interface MainState {
+    content: string,
+    authorizeUrl: string,
+    providers: string,
+    drawerActive: boolean
+}
 
 export class Main extends React.Component<{}, MainState> {
-    constructor(props: {}) {
-        super(props);
-        this.state = { content: "", authorizeUrl: "", providers: "" };
+    state = {
+        content: "",
+        authorizeUrl: "",
+        providers: "",
+        drawerActive: false
+    }
+
+    toggleDrawerActive = () => {
+        this.setState({ drawerActive: !this.state.drawerActive })
     }
 
     async componentDidMount() {
@@ -33,12 +45,14 @@ export class Main extends React.Component<{}, MainState> {
 
     render() {
         return <Layout>
-            <NavDrawer>
-                <p>Drawer</p>
+            <NavDrawer active={this.state.drawerActive} onOverlayClick={this.toggleDrawerActive}>
+                <List>
+                    <ListItem caption="Add account"></ListItem>
+                </List>
             </NavDrawer>
             <Panel>
-                <AppBar leftIcon='menu' />
-                <div>
+                <AppBar leftIcon='menu' onLeftIconClick={this.toggleDrawerActive} />
+                <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
                     <p>Google auth url: {this.state.authorizeUrl}</p>
                     <p>Content: {this.state.content}</p>
                     <p>Providers: {this.state.providers}</p>
