45c1b55c78de08a31cf66bd17252904e3b7d7dff
Paweł Wegner <pawel.wegner95@gmail.com>
2019-05-29 01:01:26 +0200

    AddAccount: cleared urls.

--- js/components/AddAccount.tsx
+++ js/components/AddAccount.tsx
@@ -9,17 +9,18 @@ interface AddAccountProps {
 };
 
 export class AddAccount extends React.Component<AddAccountProps, {}> {
-    patchUrl = (url: string) => {
+    patchUrl = (cloud: string, url: string) => {
         const m = url.match("^(.*)redirect_uri=[^&]*(.*)$");
         if (m) {
-            if (url.indexOf("hubic.com") != -1)
-                return `${m[1]}redirect_uri=${process.env.HOSTNAME}/${m[2]}`;
-            else
-                return `${m[1]}redirect_uri=${process.env.HOSTNAME}${m[2]}`;
+            const str = cloud === "hubic" ?
+                `${m[1]}redirect_uri=${process.env.HOSTNAME}/${m[2]}` :
+                `${m[1]}redirect_uri=${process.env.HOSTNAME}${m[2]}`;
+            const s = str.match("^(.*)state=[^&]*(.*)$");
+            return `${s[1]}state=${cloud}${s[2]}`;
         }
         else {
             const m = url.match(`^${process.env.HOSTNAME}/([^/]*)/login(.*)$`);
-            return `/auth/${m[1]}/${m[2]}`;
+            return `/auth/${m[1]}`;
         }
     }
 
@@ -28,11 +29,11 @@ export class AddAccount extends React.Component<AddAccountProps, {}> {
             {this.props.factory.availableProviders().map((value, _) => {
                 const url = this.props.factory.authorizeUrl(value);
                 if (url.match(`^${process.env.HOSTNAME}.*$`))
-                    return <Link key={`${value}-internal`} to={this.patchUrl(url)}>
+                    return <Link key={`${value}-internal`} to={this.patchUrl(value, url)}>
                         <ListItem caption={value} />
                     </Link>
                 else
-                    return <a key={`${value}-external`} href={this.patchUrl(url)}>
+                    return <a key={`${value}-external`} href={this.patchUrl(value, url)}>
                         <ListItem caption={value} />
                     </a>;
             })}
